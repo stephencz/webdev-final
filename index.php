751d05f52795ae9a2d17af897af12194a98eaf34
Stephen Czekalski <admin@stephencz.com>
2020-11-23 13:34:55 -0500

    Added normalize.css and skeleton.css

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Untitled Project</title>
+    <meta name="author" content="Stephen Czekalski">
+
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/skeleton.css">
 
 </head>
 <body>
 
-    <?php phpinfo() ?>
+    <div class="container">
+        <div class="row">
+            <div class="column twelve">
+                <h1>Untitled Project Test</h1>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>
