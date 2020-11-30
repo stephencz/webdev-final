e0434d3d617a886baf1a713b7a7fa6b4a74d44b7
Stephen Czekalski <admin@stephencz.com>
2020-11-30 09:09:18 -0500

    Began working on main form

--- index.php
+++ index.php
@@ -6,22 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Untitled Project</title>
+    <title>Weight Management Tool</title>
     <meta name="author" content="Stephen Czekalski">
 
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/skeleton.css">
+    <!-- Link to Bootstrap's CSS -->
+    <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
+        crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/styles.css">
 
 </head>
 <body>
 
-    <div class="container">
-        <div class="row">
-            <div class="column twelve">
-                <h1>Untitled Project Test</h1>
-            </div>
-        </div>
-    </div>
+    <?php require("includes/header.html"); ?>
+    <?php require("includes/primary-info.html"); ?>
+
+    <!-- Link to JQuery. Needed for Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" 
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" 
+        crossorigin="anonymous"></script>
 
 </body>
 </html>
